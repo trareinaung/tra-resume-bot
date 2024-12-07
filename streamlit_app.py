846cde1945f1197cac1559166623949bcb672a62
Arkar Min Aung <arica.aung@gmail.com>
2024-12-07 05:16:57 +0000

    update docs

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("👨‍💼📊📈📁 Resume question answering")
 st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
+    "Upload a resume below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
